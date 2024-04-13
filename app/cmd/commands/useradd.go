b968f90ec319fa878799572ab0bfa5804070fa0e
Christos Chousiadas <chousiadas6395@gmail.com>
2024-04-13 12:37:09 +0300

    CMD application

---
+++ app/cmd/commands/useradd.go
@@ -0,0 +1,53 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"net/mail"
+	"time"
+
+	"github.com/Housiadas/backend-system/business/data/sqldb"
+	"github.com/Housiadas/backend-system/business/domain/userbus"
+	"github.com/Housiadas/backend-system/business/domain/userbus/stores/userdb"
+	"github.com/Housiadas/backend-system/foundation/logger"
+)
+
+// UserAdd adds new users into the database.
+func UserAdd(log *logger.Logger, cfg sqldb.Config, name, email, password string) error {
+	if name == "" || email == "" || password == "" {
+		fmt.Println("help: useradd <name> <email> <password>")
+		return ErrHelp
+	}
+
+	db, err := sqldb.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	userBus := userbus.NewCore(log, nil, userdb.NewStore(log, db))
+
+	addr, err := mail.ParseAddress(email)
+	if err != nil {
+		return fmt.Errorf("parsing email: %w", err)
+	}
+
+	nu := userbus.NewUser{
+		Name:            name,
+		Email:           *addr,
+		Password:        password,
+		PasswordConfirm: password,
+		Roles:           []userbus.Role{userbus.RoleAdmin, userbus.RoleUser},
+	}
+
+	usr, err := userBus.Create(ctx, nu)
+	if err != nil {
+		return fmt.Errorf("create user: %w", err)
+	}
+
+	fmt.Println("user id:", usr.ID)
+	return nil
+}
