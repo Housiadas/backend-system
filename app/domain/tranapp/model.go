1317532523a03cf2a6f3c837808f55c54f011a1f
Housi <chousiadas6395@gmail.com>
2025-01-18 17:14:15 +0200

    Small changes

--- app/domain/tranapp/model.go
+++ app/domain/tranapp/model.go
@@ -65,7 +65,7 @@ func (app NewTran) Validate() error {
 
 // Decode implements the decoder interface.
 func (app *NewTran) Decode(data []byte) error {
-	return json.Unmarshal(data, &app)
+	return json.Unmarshal(data, app)
 }
 
 // =============================================================================
