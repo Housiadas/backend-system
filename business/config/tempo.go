cc17aa0d5783180bf1c96ddfd8c0cecc60b52ddc
Christos Chousiadas <41087223+Housiadas@users.noreply.github.com>
2025-01-19 18:26:24 +0200

    Small changes (#45)

--- business/config/tempo.go
+++ business/config/tempo.go
@@ -1,9 +1,9 @@
 package config
 
 type Tempo struct {
-	Host        string
-	Probability float64
+	Host string
 	// Shouldn't use a high Probability value in non-developer systems.
 	// 0.05 should be enough for most systems. Some might want to have
 	// this even lower.
+	Probability float64
 }
